27784784cccd38e1fa080d301d2a8c31812cac59
Idknowtf <unknownime0@gmail.com>
2026-03-27 20:18:38 +0900

    몬스터 설정 디자인(최종)

--- monster.cpp
+++ monster.cpp
@@ -3,8 +3,36 @@
 #include <ctime>     // time()
 using namespace std;
 
+// ========================
+// 몬스터 클래스
+// ========================
+class Monster {
+public:
+    string name;
+    int hp;
+    int attack;
+
+    // 몬스터 스탯 설정 함수
+    void setStats(string n, int level, int hpMin, int hpMax, int atkMin, int atkMax) {
+        name   = n;
+        hp     = (level * hpMin) + rand() % (level * (hpMax - hpMin) + 1);
+        attack = (level * atkMin) + rand() % (level * (atkMax - atkMin) + 1);
+    }
+
+    // 몬스터 정보 출력 함수
+    void printInfo() {
+        cout << "  이름: "   << name   << endl;
+        cout << "  체력: "   << hp     << endl;
+        cout << "  공격력: " << attack << endl;
+        cout << endl;
+    }
+};
+
+// ========================
+// 메인 함수
+// ========================
 int main() {
-    // 랜덤 시드 설정 (매번 다른 값이 나오게)
+    // 랜덤 시드 설정
     srand(time(0));
 
     // Null의 레벨 입력
@@ -12,28 +40,22 @@ int main() {
     cout << "Null의 레벨을 입력하세요: ";
     cin >> level;
 
-    // 몬스터 이름 배열 (약 → 강 순)
-    string monsterName[6] = { "Bug", "Leak", "Deadlock", "Overflow", "Segfault", "Undefined" };
+    // 몬스터 6마리 객체 생성
+    Monster monsters[6];
 
-    // 등급별 배율 (몬스터마다 조금씩 강해짐)
-    int hpMin[6]  = { 10, 15, 20, 27, 35, 45 };
-    int hpMax[6]  = { 15, 22, 30, 40, 52, 65 };
-    int atkMin[6] = {  3,  5,  8, 12, 17, 23 };
-    int atkMax[6] = {  5,  8, 12, 17, 23, 30 };
+    // 각 몬스터 스탯 설정 (이름, 레벨, 체력Min, 체력Max, 공격Min, 공격Max)
+    monsters[0].setStats("Bug",       level, 10, 15, 3,  5 );
+    monsters[1].setStats("Leak",      level, 15, 22, 5,  8 );
+    monsters[2].setStats("Deadlock",  level, 20, 30, 8,  12);
+    monsters[3].setStats("Overflow",  level, 27, 40, 12, 17);
+    monsters[4].setStats("Segfault",  level, 35, 52, 17, 23);
+    monsters[5].setStats("Undefined", level, 45, 65, 23, 30);
 
+    // 몬스터 정보 출력
     cout << "\n===== 몬스터 정보 =====" << endl;
-
     for (int i = 0; i < 6; i++) {
-        // 체력: 레벨 × 배율 범위 내 랜덤
-        int hp     = (level * hpMin[i])  + rand() % (level * (hpMax[i] - hpMin[i]) + 1);
-
-        // 공격력: 레벨 × 배율 범위 내 랜덤
-        int attack = (level * atkMin[i]) + rand() % (level * (atkMax[i] - atkMin[i]) + 1);
-
-        cout << "[몬스터 " << i + 1 << "] " << monsterName[i] << endl;
-        cout << "  체력: "   << hp     << endl;
-        cout << "  공격력: " << attack << endl;
-        cout << endl;
+        cout << "[몬스터 " << i + 1 << "]" << endl;
+        monsters[i].printInfo();
     }
 
     return 0;
