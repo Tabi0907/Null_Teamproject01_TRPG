572034fed0bd46691ffb2598d6639a78ef1fe3c2
언리얼\rotte <rottenfish04@gmail.com>
2026-03-27 20:00:39 +0900

    캐릭터 생성: class 선언 수정본

--- player_start/player_start/player_start.cpp
+++ player_start/player_start/player_start.cpp
@@ -3,40 +3,47 @@
 
 using namespace std; 
 
-string Name; //스트링 함수
-int Hp = 100; //hp level 기타등등 선언
-int Level = 1;
-int Attack = 30;
-int Exp = 0;
-
-void print() //나중에 한눈에 보기 위한 창
+class Character
 {
-	cout << "이름: " << Name << endl; //나중에 호츨시 볼 수 있음
-	cout << "레벨: " << Level << endl;
-	cout << "체력: " << Hp << endl;
-	cout << "공격력: " << Attack << endl;
-	cout << "경험치: " << Exp << endl;
-}
+public:
+	string Name; //스트링 함수
+	int Hp = 100; //hp level 기타등등 선언
+	int Level = 1;
+	int Attack = 30;
+	int Exp = 0;
 
-void LevelUp() //경험치가 들어올 경우 레벨 1씩 증가
-{
-	Level++;
-	Exp = 0;
-	cout << "레벨업! \n";
-}
-
-void LevelUpCheck() //if 문으로 100이 넘을 경우 위에 있는 함수 호출 후 레벨업
-{
-	if (Exp >= 100)
+	void print() //나중에 한눈에 보기 위한 창
 	{
-		LevelUp();
+		cout << "이름: " << Name << endl; //나중에 호츨시 볼 수 있음
+		cout << "레벨: " << Level << endl;
+		cout << "체력: " << Hp << endl;
+		cout << "공격력: " << Attack << endl;
+		cout << "경험치: " << Exp << endl;
 	}
-}
+
+	void LevelUp() //경험치가 들어올 경우 레벨 1씩 증가
+	{
+		Level++;
+		Exp = 0;
+		cout << "레벨업! \n";
+	}
+
+	void LevelUpCheck() //if 문으로 100이 넘을 경우 위에 있는 함수 호출 후 레벨업
+	{
+		if (Exp >= 100)
+		{
+			LevelUp();
+		}
+	}
+};
 
 int main() // 숫자값에 따른 입력창!
 {
+
+	Character player;
+
 	cout << "이름 입력: ";
-	cin >> Name; // 입력한 이름 호출
+	cin >> player.Name; // 입력한 이름 호출
 
 	while (true) // 2번 누르기 전까지 반복, 1번 누르면 상시 확인 가능
 	{
@@ -46,7 +53,7 @@ int main() // 숫자값에 따른 입력창!
 
 		if (number == 1) //1을 누를 경우 위에 선언했던 프린트창 호출
 		{
-			print();
+			player.print();
 		}
 
 		else if (number == 2) //2번 입력시 종료
