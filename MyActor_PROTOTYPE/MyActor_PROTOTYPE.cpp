fa6d5a7e3bf80268ff0ca7f71c7cada34922768b
문경호\khm99 <khm991008@gmail.com>
2026-03-31 12:16:50 +0900

    로그 출력 관련 c++ 기준 수정 완료

--- MyActor_PROTOTYPE/MyActor_PROTOTYPE.cpp
+++ MyActor_PROTOTYPE/MyActor_PROTOTYPE.cpp
@@ -1,48 +1,11 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-//로그학인을 맡고 한번 만들어봤는데 아무라도 많이 빈공간이 있는것 같지만 여기까지가 저의 최선입니다.
+﻿#include "MyActor_PROTOTYPE.h"
+#include <iostream>
 
-
-#include "MyActor_PROTOTYPE.h"
-
-// 처음에 이 액터가 만들어질 때 설정
-AMyActor_PROTOTYPE::AMyActor_PROTOTYPE()
-{
-    // 매 순간 검사하는 기능(Tick)을 켜둘게요.
-    // Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-    PrimaryActorTick.bCanEverTick = true;
-
+void MyActor_PROTOTYPE::BeginPlay() {
+    ShowMyLog("반가워요! 로그 시스템이 온라인 상태입니다.");
 }
 
-// 게임 시작!
-// Called when the game starts or when spawned
-void AMyActor_PROTOTYPE::BeginPlay()
-{
-    Super::BeginPlay();
-
-    // 시작하자마자 "로그 준비 완료"라고 화면에 띄워볼게요.
-    ShowMyLog(TEXT("반가워요! 로그 시스템이 켜졌습니다."), FColor::Yellow);
-}
-
-// 매 프레임마다 하는 일
-void AMyActor_PROTOTYPE::Tick(float DeltaTime)
-{
-    Super::Tick(DeltaTime);
-}
-
-
-// [핵심 기능] 팀원들이 부탁하면 화면에 글자를 그려주는 함수
-void AMyActor_PROTOTYPE::ShowMyLog(FString Message, FColor Color)
-{
-    // GEngine이 있을 때만(화면이 보일 때만) 실행해요.
-    if (GEngine)
-    {
-        // 화면에 글자를 띄워줍니다! (5초 동안 유지)
-        GEngine->AddOnScreenDebugMessage(-1, 5.0f, Color, Message);
-    }
-
-    // 나중에 확인하기 위해 내 보관함에도 저장해둬요.
-    SavedLogs.Add(Message);
-
-    // 개발자 창(Output Log)에도 기록을 남겨요.
-    UE_LOG(LogTemp, Warning, TEXT("로그 기록: %s"), *Message);
+void MyActor_PROTOTYPE::ShowMyLog(std::string Message) {
+    std::cout << ">> [LOG] " << Message << std::endl;
+    SavedLogs.push_back(Message);
 }
--- MyActor_PROTOTYPE/MyActor_PROTOTYPE.h
+++ MyActor_PROTOTYPE/MyActor_PROTOTYPE.h
@@ -1,34 +1,12 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
+#include <string>
+#include <vector>
 
-#include "CoreMinimal.h"
-#include "GameFramework/Actor.h"
-#include "MyActor_PROTOTYPE.generated.h"
-
-UCLASS()
-class MYPROJECT_API AMyActor_PROTOTYPE : public AActor
+class MyActor_PROTOTYPE
 {
-    GENERATED_BODY()
-
 public:
-    // 1. 처음 시작할 때 준비하는 곳
-    AMyActor_PROTOTYPE();
-
-protected:
-    // 2. 게임 시작 버튼 누르면 딱 한 번 실행되는 곳
-    virtual void BeginPlay() override;
-
-public:
-    // 3. 게임 도중 계속 실행되는 곳 (지금은 비워둘게요!)
-    virtual void Tick(float DeltaTime) override;
-
-    // --- [여기서부터 내가 만든 구간!] ---
-    // 다른 팀원들이 "로그 찍어줘!"라고 부탁할 때 쓸 함수예요.
-    // Message: 보여줄 글자 / Color: 글자 색상
-    void ShowMyLog(FString Message, FColor Color);
-
+    void BeginPlay();
+    void ShowMyLog(std::string Message);
 private:
-    // 나중에 로그를 모아서 보고 싶을 때를 대비해 저장해두는 목록이에요.
-    TArray<FString> SavedLogs;
+    std::vector<std::string> SavedLogs;
 };
